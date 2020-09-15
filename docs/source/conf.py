992a183b24b52c9ae7889dd79d4bc1310cb7f0ab
harirakul <harirakul.a@gmail.com>
2020-09-14 17:25:00 -0700

    using Sphinx RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import sphinx_rtd_theme
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -31,6 +32,7 @@ release = 'v1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
