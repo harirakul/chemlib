ac73a8aeaa64071e1a4456d065400a9bc69a2c4f
harirakul <harirakul.a@gmail.com>
2020-09-15 15:05:19 -0700

    Set pygments style to Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,6 +60,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+pygments_style = 'sphinx'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
