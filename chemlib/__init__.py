4dc7c77421150713b4139a93d70458e8ecd839a5
harirakul <harirakul.a@gmail.com>
2020-12-17 16:45:45 -0800

    resolved imports

--- chemlib/__init__.py
+++ chemlib/__init__.py
@@ -4,8 +4,7 @@ pte = PeriodicTable()
 SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
 AVOGADROS_NUMBER = 6.02e+23
 
-from chemlib.chemistry import Element, Compound, Reaction
-from chemlib.chemistry import Combustion, Solution
+from chemlib.chemistry import Element, Compound, Reaction, Combustion, Solution
 from chemlib.chemistry import empirical_formula_by_percent_comp, combustion_analysis
 
 c = 2.998*10**8    #Speed of light
@@ -15,5 +14,5 @@ R = 1.0974*10**7  #Rydberg constant
 from chemlib.quantum_mechanics import Wave, rydberg
 from chemlib.quantum_mechanics import energy_of_hydrogen_orbital
 
-from chemlib.electrochemistry import electrolysis, F
+from chemlib.electrochemistry import electrolysis, F, Galvanic_Cell
 
