20e6fdc98680459a612c6833ef6fe2ee37900313
harirakul <harirakul.a@gmail.com>
2020-12-21 20:19:50 -0800

    added tests

---
+++ tests/compounds.py
@@ -0,0 +1,19 @@
+import unittest
+from chemlib.chemistry import Compound
+
+CMPDS = {
+    "H2SO4": {"H": 2, "S": 1, "O": 4},
+    "H2O": {'H': 2, 'O': 1},
+    "SSnS": {'S': 2, 'Sn': 1},
+    "C6H12O6": {"C": 6, "H": 12, "O": 6},
+    "Al23": {"Al": 23}
+}
+
+class TestFormulae(unittest.TestCase):
+
+    def test_formulae(self):
+        for formula in CMPDS:
+            self.assertEqual(Compound(formula).occurences, CMPDS[formula], msg = formula)
+
+if __name__ == "__main__":
+    unittest.main()
