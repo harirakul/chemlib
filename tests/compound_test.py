8f3eee46a3ac374b3ed0a10384c14223f0033a84
harirakul <harirakul.a@gmail.com>
2021-01-06 15:59:38 -0800

    Added formula test cases

--- tests/compound_test.py
+++ tests/compound_test.py
@@ -1,11 +1,19 @@
 import unittest
+
+import sys
+sys.path.insert(1, '../chemlib')
+
 from chemlib.chemistry import Compound
 
 CMPDS = {
     "H2SO4": {"H": 2, "S": 1, "O": 4},
     "H2O": {'H': 2, 'O': 1},
     "C6H12O6": {"C": 6, "H": 12, "O": 6},
-    "Al23": {"Al": 23}
+    "Al23": {"Al": 23},
+    "MnO4": {"Mn" : 1, "O": 4},
+    "Mg(SO4)2": {"Mg": 1, "S":2, "O":8},
+    "(Al)3(SO4)2": {"Al": 3, "S":2, "O":8},
+    "MnSMn": {"Mn": 2, "S":1}
 }
 
 class TestFormulae(unittest.TestCase):
